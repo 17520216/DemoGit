a9a3eb3cfe01d316b3c5d4dfae650bcd8ba5a50b
annguyen1511 <annguyen1511>
2022-03-23 20:58:38 +0700

    set up react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        Learning React Js
+    </div>
+  )
+}
+
+export default App
